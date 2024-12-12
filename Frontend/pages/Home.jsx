6db286134ea0d627325e38f2e4a1d5379812ae36
Tejas Najare <145053380+Macdox@users.noreply.github.com>
2024-12-12 15:36:04 +0530

    route

---
+++ Frontend/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../src/componets/Navbar'
+
+function Home() {
+  return (
+    <>
+    <div>Home</div>
+    <Navbar/>
+    </>
+  )
+}
+
+export default Home
