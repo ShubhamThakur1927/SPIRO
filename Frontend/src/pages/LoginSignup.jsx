2bd177925a263fd0a734d975876a66aa86c42f60
Tejas Najare <145053380+Macdox@users.noreply.github.com>
2024-12-13 00:30:01 +0530

    login updated

--- Frontend/src/pages/LoginSignup.jsx
+++ Frontend/src/pages/LoginSignup.jsx
@@ -4,19 +4,36 @@ import ReactDOM from 'react-dom/client';
 function LoginSignup() {
  const [name, setName] = useState("");
   return (
-    <div className="h-screen w-screen flex font-rethink-sans">
-      <div className="bg-[#EAEAEA] w-1/2"></div>
-      <div className="bg-[#020817] w-1/2 text-white grid text-center align-middle items-center text-2xl">
-        {/*<img src={} alt="" />*/}
-        <h1 className='relative top-16 '>Logo</h1>
-        <div className="flex flex-col border-solid border-2 text-center mx-[186px] rounded-2xl border-[#27272A] w-[387px] h-[507px] py-8 ">
-            <h1 className='underline'>Login</h1>
-            <form className='p-8 text-center '>
-                <label className=''>Enter Your name
-                <input type="text" />
-                </label>
+    <div className="w-full h-screen flex items-start font-rethink-sans">
+      <div className="relative w-1/2 h-full flex flex-col bg-[#EAEAEA]">
+      </div>
+      <div className="bg-[#020817] relative w-1/2 h-full flex flex-col p-10 text-white text-center justify-between">
+        <h1 className="text-xl text-center py-10">[Brand]</h1>
+        <div className="w-full flex flex-col mx-5">
+          <div className='w-full flex flex-col mb-10'>
+          {/* Login */}
+          <h3 className='text-2xl font-semibold mb-4 '>Login</h3>
+          {/* Subtitle */}
+          <p className='text-lg mb-2'>Welcome Back! Please Enter you details.</p>
+          </div>
+          <div className='w-full flex flex-col'>
+            <form>
+              <input type="email"
+              placeholder='Enter your email'
+              className='w-full py-4 my-2 text-white border-b  border-white font-extrabold bg-transparent outline-none focus:outline-none' />
+
+              <input type="password"
+              placeholder='Enter your password'
+              className='w-full py-4 my-2 text-white border-b  border-white font-extrabold bg-transparent outline-none focus:outline-none' />
             </form>
-        </div>
+          </div>
+
+          </div>
+
+          {/* Don't Have Account */}
+          <div className='w-full items-center justify-center'>
+            <p className='text-sm'>Don't have an account? <span className='underline font-extrabold underline-offset-2 cursor-pointer'>Sign Up</span></p>
+          </div>
       </div>
     </div>
   );
