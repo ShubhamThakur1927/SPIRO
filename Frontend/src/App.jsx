70b13e715d76bc8ff92459a2c71cbcc6def9f284
Tejas Najare <145053380+Macdox@users.noreply.github.com>
2024-12-13 17:54:34 +0530

    SIGN UP ADDED

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import LoginSignup from './pages/LoginSignup'
-
+import Signup from './pages/Signup'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -12,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginSignup />} />
+        <Route path='/signup' element={<Signup />} />
       </Routes>
     </BrowserRouter>
   )
