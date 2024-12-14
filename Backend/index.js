c5b8ed84e390e3975ae7e16e2dc9d14f2c175727
Tejas Najare <145053380+Macdox@users.noreply.github.com>
2024-12-14 20:37:54 +0530

    backend for sign up

--- Backend/index.js
+++ Backend/index.js
@@ -1,29 +1,35 @@
-import express from 'express';
-import morgan from 'morgan';
-import dotenv from 'dotenv';
-import connectDB from './Config/db.js';
-import authRoutes from './routes/authRoutes.js';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
+
+import { connectDB } from "./db/connectDB.js";
+
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
-connectDB();
+const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-const app = express()
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
-app.use(express.json());
-app.use(morgan('dev'));
+app.use(express.json()); // allows us to parse incoming requests:req.body
+app.use(cookieParser()); // allows us to parse incoming cookies
 
-app.use('/api/v1/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
-app.get('/',(req,res) => {
-    res.send({
-        message:'welcome to Spiro'
-    })
-})
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-const PORT = process.env.PORT || 8080;
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
-app.listen(PORT, () =>{
-    console.log(`Server running on ${PORT}`);
+app.listen(PORT, () => {
+	connectDB();
+	console.log("Server is running on port: ", PORT);
 });
-
