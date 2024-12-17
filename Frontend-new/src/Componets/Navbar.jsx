d92889d5a68d61d59713048eab149c52e880b071
Tejas Najare <145053380+Macdox@users.noreply.github.com>
2024-12-17 16:33:40 +0530

    add

--- Frontend-new/src/Componets/Navbar.jsx
+++ Frontend-new/src/Componets/Navbar.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Menu } from 'lucide-react'
 import Search from './Search'
+import { Link,Outlet } from 'react-router-dom'
+import '../Pages/Styles/Home.css'
 
 function Navbar() {
   return (
-    <div className='flex justify-between items-center align-middle w-full h-auto'>
-        <div className='Logo'><span className='font-semibold text-2xl text-[#0056D2]'>SPIRO</span></div>
+    <div className='flex justify-between items-center align-middle w-full h-auto source-sans'>
+        <Link to="/"><div className='Logo'><span className='font-semibold text-2xl text-[#0056D2]'>SPIRO</span></div></Link>
         <div className="search hidden md:grid"><Search/></div>
         <div className="list font-semibold hidden md:flex">
             <ul className='flex gap-10 items-center align-middle'> 
-                <li className='hover:text-[#0056D2]'>Courses</li>
-                <li  className='hover:text-[#0056D2]'>About</li>
-                <li className='hover:text-[#0056D2]'>Contact us</li>
-                <li><button className='action-btn bg-[#0056D2] text-white py-2 px-6 rounded-full'>Login</button></li>
+                <Link to="*" ><li className='hover:text-[#0056D2]'>Courses</li></Link>
+                <Link to="*" ><li  className='hover:text-[#0056D2]'>About</li></Link>
+                <Link to="*" ><li className='hover:text-[#0056D2]'>Contact us</li></Link>
+                <Link to="*" ><li><button className='action-btn hover:bg-[#3f74bf] bg-[#0056D2] text-white py-2 px-6 rounded-full'>Login</button></li></Link>
             </ul>
         </div>
         <div className="toggle-btn md:hidden">
--- Frontend-new/src/Pages/Home.jsx
+++ Frontend-new/src/Pages/Home.jsx
@@ -2,25 +2,39 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../Componets/Navbar'
 import Korean from '../assets/korean.png'
+import { ArrowRight } from 'lucide-react';
+import './Styles/Home.css'
 function Home() {
   return (
-    <div className='bg[#f7f7f8] h-screen'>
+    <>
+    <section className='h-full w-auto  bg-[#F7F7F8] source-sans'>
+    <div className='h-auto w-auto my-0 mx-auto p-8 text-center max-w-7xl bg-[#F7F7F8] source-sans'>
       <Navbar/>
       <section className='Hero md:flex mt-12 grids w-full'>
           <div className='md:w-1/2 w-full'>
-          <div className='md:w-auto w-full my-12 flex md:hidden'>
-            <img src={Korean} alt="girls" className='w-screen pl-32' />
-          </div>
           <div  className='text-start pt-16'>
                 <h1 className='text-5xl font-semibold mb-8'>Learn Without Limits</h1>
-                <p className='text-lg'>Start, switch, or advance your career with more than 7,000 courses, Professional Certificates, and degrees from world-class universities and companies.</p>
+                <p className='text-[#4B4B4B] pr-10 md:pr-28'>Start, switch, or advance your career with more than 7,000 courses, Professional Certificates, and degrees from world-class universities and companies.</p>
+                
+                <div className='flex mt-8 gap-4'>
+                <button className='action-btn bg-[#0056D2] hover:bg-[#3f74bf] text-white py-2 px-6 rounded-full'>Get Started</button>
+                <button className='action-btn bg-transparent border-2 border-[#0056D2] text-[#0056D2] py-2 px-6 rounded-full flex w-auto'>Learn More <ArrowRight /></button>                
+                </div>
+                <h3 className='m-6 text-base'>Trusted Partners</h3>
+                <div className='flex gap-4 m-6'>
+                  [partners-images]
                 </div>
           </div>
+          </div>
           <div className='md:w-1/2 w-full my-12 hidden md:flex'>
-            <img src={Korean} alt="girls" className='w-screen pl-32' />
+            <img src={Korean} alt="girls" className='w-fit pt-7 pl-32 relative bottom-10' />
           </div>
       </section>
     </div>
+    </section>
+    <section className="h-lvh ">
+    </section>
+    </>
   )
 }
 
