d8783d467f62fbb78cb69cee040dcd9c965333b2
Tejas Najare <145053380+Macdox@users.noreply.github.com>
2025-01-08 17:52:38 +0530

    tailwind setup done

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,27 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'Inter' : ['Inter', 'sans-serif'],
+        'Sourcesans' : ['Source Sans 3', 'sans-serif'],
+      },
+      fontSize:{
+        'display': "64px",
+        'small':"20px",
+        'h4': "24px",
+        'h2':"36px"
+      },
+      lineHeight:{
+        'h2':"48px"
+      },
+
+      colors: {
+        'primary' : "#0065F7",
+        'secondary' : "#F2F2F2",
+        'Dark' : "#020817",
+      }
+    },
   },
   plugins: [],
 }
