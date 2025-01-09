8ebe633d55ea76c4614417cbf2d86832d9e55299
Deep-0610 <deepsc0606@gmail.com>
2025-01-09 17:49:26 +0530

    Smallcard & Card

---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Card (props) {
+  return (
+    <div className="card bg-card h-[424px] w-[388px] rounded-xl ">
+      <div className="card-content relative top-3/4 p-6 ">
+        <h3 className="title text-h3 font-semibold">{props.title}</h3>
+        <p className="subtitle text-p">{props.subtitle}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card
