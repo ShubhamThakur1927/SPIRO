a671257180176d51a92d472eab3b790ddb3e0ff4
Deep-0610 <deepsc0606@gmail.com>
2025-01-25 12:12:27 +0530

    Cardoftheday

--- frontend/src/pages/Testubg.jsx
+++ frontend/src/pages/Testubg.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import Chemistrycard from '../Tests/Components/Chemistrycard'
 import Circulargraphcard from '../Tests/Components/Circulargraphcard'
+import Cardoftheday from '../Tests/Components/Cardoftheday'
 
 function Testubg() {
   return (
     <div className="p-6 space-y-4">
-      
+      <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
 
-      {/* Example Usage */}
-      <Circulargraphcard
-        title="Eng. Mathematics - IV"
-        percentage={75} // Pass percentage dynamically
-        isActive={true} // Set active or inactive state
-        width="400px"
+     
+
+      {/* Example 2: Custom Dimensions */}
+      <Cardoftheday
+        title="Total no. of Students"
+        value="1200"
+        width="450px"
         height="200px"
       />
-      
     </div>
   )
 }
