b888bfac31bb62e366a86ad561cfc36c6cb58b1b
Deep-0610 <deepsc0606@gmail.com>
2025-01-25 11:53:10 +0530

    Graphcard

--- frontend/src/pages/Testubg.jsx
+++ frontend/src/pages/Testubg.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import Chemistrycard from '../Tests/Components/Chemistrycard'
+import Circulargraphcard from '../Tests/Components/Circulargraphcard'
 
 function Testubg() {
   return (
     <div className="p-6 space-y-4">
-      <h1 className="text-2xl font-bold mb-4">Chemistry Modules</h1>
-      <Chemistrycard
-        image="https://s3-alpha-sig.figma.com/img/a233/1764/44ce94f91d005b9f4086800bd4b04a93?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=kW8QzKnSKNT4cU8Ud~3C8-uLagC-yDafa2ePIv54~b~p4X1-w1SCmROhsk7Wv~m1ojVAWIcDxu2nSJrHb7NcKQK-vo6bhFn7wijezoKVuK6-qE66J7IW0a0UfuN4qbCS~CxwnnlBxEMA1QTBB3rBHZyv61du7c6o89auJM6nmCS8A~NZ28L6p8Zdv4ZgKpPvu5w9hvjQ8B9aehezmJ5ke-GrcRaWctgGFMV7Jedob6s-z2trGgU86psSKE4k0GctGzsXc3YGNy4t5YQqh0EvauC21mLopKLCAHQKAT6r~3la~EghYRZlgw0h31KzWgtbgirsRslWBjBZU1pAzf1m0g__"
-        title="CHEMISTRY"
-        author="Prof. Shravani"
-        
+      
+
+      {/* Example Usage */}
+      <Circulargraphcard
+        title="Eng. Mathematics - IV"
+        percentage={75} // Pass percentage dynamically
+        isActive={true} // Set active or inactive state
+        width="400px"
+        height="200px"
       />
+      
     </div>
   )
 }
