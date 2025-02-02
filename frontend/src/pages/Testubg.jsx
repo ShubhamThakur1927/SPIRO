7137386bb0e41790f300f6b9c02808754851e7ce
Deep-0610 <deepsc0606@gmail.com>
2025-02-02 11:28:22 +0530

    404 Error

--- frontend/src/pages/Testubg.jsx
+++ frontend/src/pages/Testubg.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import Aboutuspage from '../Tests/Components/Aboutuspage'
-import Profilepage from '../Tests/Components/Profilepage'
-import Termsandcondition from '../Tests/Components/Termsandcondition'
-import AboutUs from './AboutUs'
+
+import Pagenotfound from '../Tests/Components/Pagenotfound'
 
 function Testubg() {
   return (
     <div>
-      <Aboutuspage/>
+      <Pagenotfound/>
     </div>
   )
 }
