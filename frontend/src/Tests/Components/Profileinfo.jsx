a03887eeeea708322351265c6e17bef5722018e6
Tejas Najare <145053380+Macdox@users.noreply.github.com>
2025-02-05 21:57:12 +0530

    minnor changes

--- frontend/src/Tests/Components/Profileinfo.jsx
+++ frontend/src/Tests/Components/Profileinfo.jsx
@@ -38,10 +38,10 @@ function Profileinfo({ details, onEdit }) {
                 value={editValues.name}
                 onChange={(e) => handleChange("name", e.target.value)}
                 placeholder={details.name}
-                className="ml-2 bg-transparent outline-none overflow-hidden"
+                className="ml-2 bg-transparent outline-none overflow-hidden "
               />
             ) : (
-              <span className="font-semibold text-gray-700 ml-2">{details.name}</span>
+              <span className="font-semibold text-gray-700 ml-2 overflow-hidden">{details.name}</span>
             )}
           </p>
         </div>
