9cdc1137cda8f5baf615e8cfea88c0d661cefbe9
Tejas Najare <145053380+Macdox@users.noreply.github.com>
2025-02-05 22:29:47 +0530

    push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -10,24 +10,13 @@ import Testubg from "./pages/Testubg";
 import LecturesView from "./pages/LecturesView";
 import AboutUs from "./pages/AboutUs";
 import PageNotFound from "./pages/PageNotFound";
-import Loader from "./components/Loader";
-
 
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated, checkAuth } = useAuthstore();
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const authenticate = async () => {
-      await checkAuth();
-      setLoading(false);
-    };
-    authenticate();
+    checkAuth();
   }, [checkAuth]);
-
-  if (loading) {
-    return <Loader />; // Show loader while checking authentication
-  }
 
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
@@ -37,19 +26,6 @@ const ProtectedRoute = ({ children }) => {
 
 const RedirectAuthenticatedUser = ({ children }) => {
   const { isAuthenticated } = useAuthstore();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const authenticate = async () => {
-      await checkAuth();
-      setLoading(false);
-    };
-    authenticate();
-  }, [checkAuth]);
-
-  if (loading) {
-    return <Loader />; // Show loader while checking authentication
-  }
 
   if (isAuthenticated) {
     return <Navigate to="/dashboard" replace />;
@@ -60,20 +36,6 @@ const RedirectAuthenticatedUser = ({ children }) => {
 
 const RedirectAuthenticatedStudent = ({ children }) => {
   const { isAuthenticated, student } = useAuthstore();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const authenticate = async () => {
-      await checkAuth();
-      setLoading(false);
-    };
-    authenticate();
-  }, [checkAuth]);
-
-  if (loading) {
-    return <Loader />; // Show loader while checking authentication
-  }
-
   if (isAuthenticated && !student.isVerified) {
     return <Navigate to="/dashboard" replace />;
   }
